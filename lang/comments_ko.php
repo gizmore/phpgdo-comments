82c46c717243bfc9f805a4382be9791009f43e47
gizmore <gizmore@wechall.net>
2026-09-18 04:05:38 +0200

    'more korean'

---
+++ lang/comments_ko.php
@@ -0,0 +1,45 @@
+<?php
+namespace GDO\Comments\lang;
+
+return [
+	'gdo_comment' => '댓글',
+	'link_comments' => '%s개의 댓글…',
+	'btn_write_comment' => '댓글 쓰기',
+	'commented_at' => '%s의 댓글',
+	'comment_top' => '상위댓글?',
+	'comment_approved' => '승인 시간',
+	'comment_approvor' => '승인자',
+	'err_comment_object_not_given' => '주석 테이블 %s에 `gdoCommentObject()` 테이블이 없습니다.',
+	'cfg_comment_email' => '새 댓글에 대해 이메일을 보내시겠습니까?',
+	'cfg_comment_approval' => '댓글 승인 엔진?.',
+	'msg_comment_added' => '귀하의 의견이 추가되었으며 공개됩니다.',
+	'msg_comment_added_approval' => '귀하의 댓글이 추가되었지만 표시되기 전에 승인이 필요합니다.',
+	'mt_comment_edit' => '댓글 수정',
+	'msg_comment_edited' => '댓글이 수정되었습니다.',
+	'msg_comment_approved' => '댓글이 승인되었습니다.',
+	'err_comment_already_approved' => '댓글이 이미 승인되었습니다.',
+	'list_comments' => '%s개의 댓글',
+	'mail_new_comment_title' => '[%s] 새 댓글',
+	'mail_approved_comment_title' => '[%s] 댓글이 승인되었습니다.',
+	'cfg_comment_approval_guest' => '손님에 대한 이메일 승인?',
+	'cfg_comment_approval_member' => '회원의 이메일 승인?',
+	'cfg_comment_captcha_guest' => '손님을 위한 보안문자?',
+	'cfg_comment_captcha_member' => '회원을 위한 보안문자?',
+	'mt_comments_admin' => '댓글 관리',
+	'mt_list_comments' => '%s에 대한 댓글',
+	'mt_comments_delete' => '댓글 삭제',
+	'msg_comment_deleted' => '댓글이 삭제된 것으로 표시되었습니다.',
+	'mail_deleted_comment_title' => '댓글이 삭제되었습니다 [%s]',
+	'err_comment_already_deleted' => '이 댓글은 이미 삭제된 것으로 표시되었습니다.',
+	'mt_comments_edit' => '댓글 수정',
+	'add_comment' => '댓글 추가',
+	'mails_new_comment' => '새 댓글',
+	'mailb_new_comment' => '친애하는 %s님,
+        %s에 새로운 댓글이 작성되었습니다.
+        보낸 사람: %s
+        주제: %s
+        메시지: %s
+        
+        감사합니다,
+        %2$s 팀',
+];
